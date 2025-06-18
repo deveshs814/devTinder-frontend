4e674428e9929dd92f581cde7259cfa053fb8922
Devesh Shukla <shukladevesh9814@gmail.com>
2025-06-18 22:49:54 +0530

    fixing the body components

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,8 +1,28 @@
-import Footer from './Footer'
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import Footer from './Footer';
+import Navbar from './Navbar';
+import axios from 'axios';
+import {BASE_URL} from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useEffect } from 'react';
 
 const Body = () => {
+  const dispatch = useDispatch();
+
+  const fetchUser = async() =>{
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view",{
+        withCredentials:true,
+      });
+      dispatch(addUser(res.data));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() =>{
+    fetchUser();
+  },[]);
+
   return (
     <>
       <Navbar/>
