5e7eadb622d8b889ef8e6bcc50a8fe2542722db3
Devesh Shukla <shukladevesh9814@gmail.com>
2025-06-19 23:09:34 +0530

    fixed some login issues and routing.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
@@ -7,49 +8,55 @@ const Navbar = () => {
     <>
       <div className="navbar bg-base-200 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
+          <Link to={"/"} className="btn btn-ghost text-xl">DevTinder</Link>
         </div>
-        <div className="flex gap-2">
-          {/* <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          /> */}
-          {user && (<div className="dropdown dropdown-end mx-5">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="user photo"
-                  src="{user.photoUrl}"
-                />
+
+        <div className="flex items-center gap-3 mx-5">
+          {user && (
+            <>
+              <span className="text-sm font-medium hidden md:block">
+                Welcome, {user.firstName}
+              </span>
+
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="user photo"
+                      src={user.photoUrl}
+                      className="object-cover w-full h-full rounded-full"
+                    />
+                  </div>
+                </div>
+
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                >
+                  <li>
+                    <Link to={"/profile"} className="justify-between">
+                      Profile
+                      <span className="badge">New</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <a>Settings</a>
+                  </li>
+                  <li>
+                    <a>Logout</a>
+                  </li>
+                </ul>
               </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>)}
+            </>
+          )}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
