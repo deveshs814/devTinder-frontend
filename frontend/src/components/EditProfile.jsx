828185c26f765ed072586d67c0cfbebd58bb650a
Devesh Shukla <shukladevesh9814@gmail.com>
2025-06-26 22:32:12 +0530

    Created the signup page and fixed bugs.

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -8,10 +8,10 @@ import { addUser } from "../utils/userSlice";
 const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
-  const [age, setAge] = useState(user.age);
+  const [age, setAge] = useState(user.age || "");
   const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
-  const [gender, setGender] = useState(user.gender);
-  const [about, setAbout] = useState(user.about);
+  const [gender, setGender] = useState(user.gender|| "");
+  const [about, setAbout] = useState(user.about|| "");
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const [showToast, setShowToast] = useState(false);
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,56 +1,97 @@
-import  { useState } from "react";
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
-import {useNavigate} from "react-router-dom";
-import {BASE_URL} from "../utils/constants.js";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants.js";
 
 const Login = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-const handleLogin = async () => {
-  try {
-    const res = await axios.post(
-      BASE_URL + "/login",
-      { emailId, password },
-      { withCredentials: true }
-    );
-    dispatch(addUser(res.data));
-    return navigate("/");
-  } catch (error) {
-    setError(error?.response?.data || "Something went wrong!!");
-  }
-};
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        { emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (error) {
+      setError(error?.response?.data || "Something went wrong!!");
+    }
+  };
 
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (error) {
+      setError(error?.response?.data || "Something went wrong!!");
+    }
+  };
   return (
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-sm m-16">
         <div className="card-body px-10">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "SignUp"}
+          </h2>
           <div>
-            <label className="input">
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <circle cx="11" cy="11" r="8"></circle>
-                <path d="m21 21-4.3-4.3"></path>
-              </g>
-              <input 
-              type="search" 
-              value={emailId} 
-              className="grow" 
-              onChange={(e)=>setEmailId(e.target.value)}
-              placeholder="E-mail" />
-            </label>
-
+            {!isLoginForm && (
+              <>
+                <label className="input">
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <path d="m21 21-4.3-4.3"></path>
+                  </g>
+                  <input
+                    type="search"
+                    value={firstName}
+                    className="grow"
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder="Firstname"
+                  />
+                </label>
+                <label className="input mt-4">
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+                    <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+                  </g>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="grow"
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder="Lastname"
+                  />
+                </label>
+              </>
+            )}
             <label className="input mt-4">
               <g
                 strokeLinejoin="round"
@@ -62,18 +103,51 @@ const handleLogin = async () => {
                 <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
                 <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
               </g>
-              <input 
-              type="text"
-              value={password}
-              className="grow" 
-              onChange={(e)=>setPassword(e.target.value)}
-              placeholder="Password" />
+              <input
+                type="text"
+                value={emailId}
+                className="grow"
+                onChange={(e) => setEmailId(e.target.value)}
+                placeholder="E-mail"
+              />
+            </label>
+            <label className="input mt-4">
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+                <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+              </g>
+              <input
+                type="text"
+                value={password}
+                className="grow"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
             </label>
           </div>
           <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary" onClick={handleLogin}>Submit</button>
+            <button
+              className="btn btn-primary"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+            >
+              Login
+            </button>
           </div>
+          <p
+            className="text-red-500 m-auto cursor-pointer"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User? Sign Up here"
+              : "Existing User ? Login Here"}
+          </p>
         </div>
       </div>
     </div>
